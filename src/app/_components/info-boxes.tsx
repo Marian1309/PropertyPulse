54c9b8e9516863bea7f92eba5f51d50cc38e17bb
Marian <pidchashymaryan@gmail.com>
2024-07-17 17:15:02 +0300

    refactor(info-boxes): removed info-box component because it is not used anywhere else

--- src/app/_components/info-boxes.tsx
+++ src/app/_components/info-boxes.tsx
@@ -1,36 +1,45 @@
 import type { FC } from 'react';
 
-import { InfoBox } from '../../components/ui';
+import Link from 'next/link';
 
 const InfoBoxes: FC = () => {
   return (
     <section>
       <div className="container-xl m-auto lg:container">
         <div className="grid grid-cols-1 gap-4 rounded-lg p-4 md:grid-cols-2">
-          <InfoBox
-            buttonInfo={{
-              text: 'Browse Properties',
-              link: '/properties',
-              backgroundColor: 'bg-black'
-            }}
-            heading="For Renters"
-          >
-            Find your dream rental property. Bookmark properties and contact
-            owners.
-          </InfoBox>
+          <div className="rounded-lg bg-gray-100 p-6 shadow-md">
+            <h2 className="text-2xl font-bold text-gray-800">For Renters</h2>
 
-          <InfoBox
-            backgroundColor="bg-blue-100"
-            buttonInfo={{
-              text: 'Add Property',
-              link: '/properties/add',
-              backgroundColor: 'bg-blue-500'
-            }}
-            heading="For Property Owners"
-          >
-            List your properties and reach potential tenants. Rent as an airbnb
-            or long term.
-          </InfoBox>
+            <p className="mb-4 mt-2 text-gray-800">
+              Find your dream rental property. Bookmark properties and contact
+              owners.
+            </p>
+
+            <Link
+              className="inline-block rounded-lg bg-black px-4 py-2 text-white hover:opacity-80"
+              href="/properties"
+            >
+              Browse Properties
+            </Link>
+          </div>
+
+          <div className="rounded-lg bg-blue-100 p-6 shadow-md">
+            <h2 className="text-2xl font-bold text-gray-800">
+              For Property Owners
+            </h2>
+
+            <p className="mb-4 mt-2 text-gray-800">
+              List your properties and reach potential tenants. Rent as an
+              airbnb or long term.
+            </p>
+
+            <Link
+              className="inline-block rounded-lg bg-blue-500 px-4 py-2 text-white hover:opacity-80"
+              href="/properties/add"
+            >
+              Add Property
+            </Link>
+          </div>
         </div>
       </div>
     </section>
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,3 @@
 export { default as GoogleButton } from './google-button';
-export { default as InfoBox } from './info-box';
 export { default as PropertyCard } from './property-card';
 export { default as Spinner } from './spinner';
